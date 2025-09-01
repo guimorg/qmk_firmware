cc2df287c8eb5705ae440a0416f9e94f4fe13fcb
Guilherme de Amorim <thexuh@MacBook-Pro.local>
2025-08-31 22:04:46 -0300

    :sparkle: first keyboard with animation
    - TODO: set backlight

---
+++ keyboards/sofle/keymaps/guimorg/config.h
@@ -0,0 +1,14 @@
+// Copyright 2024 Santosh Kumar (@santosh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define TRI_LAYER_LOWER_LAYER 1
+#define TRI_LAYER_UPPER_LAYER 2
+#define TRI_LAYER_ADJUST_LAYER 3
+#define TAPPING_TERM 230
+#define PERMISSIVE_HOLD
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define OLED_TIMEOUT 120000
+#define OLED_BRIGHTNESS 120
+#define SPLIT_WPM_ENABLE
